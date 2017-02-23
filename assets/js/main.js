ba80c70e13d3c005e56047369c8f776cecc09a6d
Daguiana <darevolledo@hotmail.com>
2017-02-23 12:33:15 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var nombre =prompt("Ingrese nombre");
+document.getElementById("name").innerHTML = nombre;
+
+var ape=prompt("Ingrese apellido");
+document.getElementById("lastname").innerHTML = ape;
+
+ var Dni=prompt("Ingrese DNI");
+ document.getElementById("dni").innerHTML = Dni;
+
+ var direc=prompt("Ingrese dirección");
+ document.getElementById("address").innerHTML = direc;
+//    document.write("* Apellido: " + ape + "<br>")
+//    document.write("* DNI: " + dni + "<br>")
+//    document.write("*Dirección: " + direc)
